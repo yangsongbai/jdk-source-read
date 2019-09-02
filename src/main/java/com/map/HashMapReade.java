bb394b0d9706cb4c6ae024398f236df4eddb2db2
yangsongbai <yangsongbai@100tal.com>
2019-09-02 09:36:28 +0800

    添加源码

---
+++ src/main/java/com/map/HashMapReade.java
@@ -0,0 +1,20 @@
+package com.map;
+
+import java.util.HashMap;
+
+/**
+ * HashMap 的主要职责:
+ *
+ * @ClassName HashMap
+ * @Description: TODO
+ * @Author songbai yang
+ * @Date 2019/9/2 7:51
+ * @Version V1.0
+ **/
+public class HashMapReade {
+
+    public void readeHashMap(){
+        HashMap<String,String> h =  new HashMap<>();
+    }
+
+}
